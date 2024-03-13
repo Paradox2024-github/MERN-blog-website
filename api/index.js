6eac3d8ba6e8d5b4cbac2d2acfeef6377b865682
paradox <paradoxmailme@gmail.com>
2024-03-14 02:19:04 +0600

    connect  to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+const db= process.env.MONGO;
+console.log(db)
+
+mongoose.connect(db).then(()=>{
+    console.log("connection done")
+}).catch((err)=>{
+    console.log(err)
+})
 
 const app = express();
 app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3000!");
 })
